3f020558edda00abb808c57cfa0b9cc2358d7421
Abel mekonn <abelmekonn9@gmail.com>
2025-03-08 13:21:17 +0300

    feat: implement loading state with Loader component and restructure RootLayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import VisitUs from "@/components/homePage/VisitUs";
 import WhyChooseUs from "@/components/homePage/WhyChooseUs";
 import YourDentist from "@/components/homePage/YourDentist";
 import Loader from "@/components/Loader";  // Import the Loader component
-import Layout from "@/components/layout/Layout";
 import HeroSection from "@/components/Hero";
 
 export default function Home() {
@@ -31,29 +30,27 @@ export default function Home() {
         <Loader />  // Show loader only on first visit
       ) : (
         <>
-          <Layout>
-            <HeroSection
-              title="Your Trusted Classic Specialty"
-              subtitle={
-                <>
-                  <span className="block md:mb-4 mb-2">Experience top-quality</span>
-                  <span className="block">dental care with a gentle touch.</span>
-                </>
-              }
-              buttonText="Request Appointment"
-              backgroundImage="/images/jose-vazquez-4SUyx4KQ5Ik-unsplash.jpg"
-              phoneNumbers={["+251909696945", "+251909696946"]}
-              location="Bisrate Gebrale, Adot Cinema, Addis Ababa"
-            />
+          <HeroSection
+            title="Your Trusted Classic Specialty"
+            subtitle={
+              <>
+                <span className="block md:mb-4 mb-2">Experience top-quality</span>
+                <span className="block">dental care with a gentle touch.</span>
+              </>
+            }
+            buttonText="Request Appointment"
+            backgroundImage="/images/jose-vazquez-4SUyx4KQ5Ik-unsplash.jpg"
+            phoneNumbers={["+251909696945", "+251909696946"]}
+            location="Bisrate Gebrale, Adot Cinema, Addis Ababa"
+          />
 
-            <About />
-            <Services />
-            <ContactUs />
-            <YourDentist />
-            <WhyChooseUs />
-            <Testimony />
-            <VisitUs />
-          </Layout>
+          <About />
+          <Services />
+          <ContactUs />
+          <YourDentist />
+          <WhyChooseUs />
+          <Testimony />
+          <VisitUs />
         </>
       )}
     </>
